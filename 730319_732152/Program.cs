7d82387aa3743d43dab904805f0265e8763694fc
visualstudio <visualstudio@122-PC29>
2019-03-08 19:34:06 -0500

    lab002

--- 730319_732152/Program.cs
+++ 730319_732152/Program.cs
@@ -8,8 +8,38 @@ namespace _730319_732152
 {
     class Program
     {
-        static void Main(string[] args)
+        public delegate void MyDelegate();
+        void Method()
         {
+            Console.WriteLine("Method1");
+            Console.ReadLine();
+        }
+
+        public void Method2()
+        {
+            Delegate Delegate = new MyDelegate(Method1);
+            myDelegate(50);
+        }
+
+        private void Method1()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void myDelegate(int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+namespace DelegatesAndEvents
+{
+ class Program
+{ 
+static void Main(string[] args)
+{
+    // TO DO: Call Method 2 from the Delegate Exercises Class
         }
     }
 }
