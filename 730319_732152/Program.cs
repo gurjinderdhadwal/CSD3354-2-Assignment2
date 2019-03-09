8160af75735ecbf9acc5d38e34603888ce600db1
visualstudio <visualstudio@122-PC29>
2019-03-08 19:20:17 -0500

    lab001

---
+++ 730319_732152/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _730319_732152
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
